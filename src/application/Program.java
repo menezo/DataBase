817b366967edf39a8224f2e118473e21a3657688
Joao Menezes <joaovitormenezesm12@gmail.com>
2022-10-22 12:04:16 -0300

    Atualizar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,8 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -14,7 +11,6 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 		
@@ -22,39 +18,19 @@ public class Program {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ?"
+					+ "WHERE "
+					+ "(DepartmentID = ?)");
 			
-			/* COMO INSERIR MAIS DE UM VALOR DE UMA VEZ
-			st = conn.prepareStatement(
-					"INSERT INTO department (Name) values ('D1'), ('D2')",
-					Statement.RETURN_GENERATED_KEYS);*/
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			}
-			else {
-				System.out.println("No rows affected");
-			}
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		catch(ParseException e) {
 			e.printStackTrace();
 		}
 		finally {
